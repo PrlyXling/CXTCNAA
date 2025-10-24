be6940f407d87cbf469ec7947b713cb337107ff9
Xling <prly.zero@gmail.com>
2025-10-25 00:00:34 +0800

    Change curl command to use POST with data encoding

--- neo_script.sh
+++ neo_script.sh
@@ -20,7 +20,7 @@ function login() {
     if [[ ! $redirect_url =~ ^$pass_url ]];then
         if [[ $redirect_url =~ ^$auth_url ]];then
             paramstr=$(urldecode "$(curl -Ls "$redirect_url" | sed -n 's/.*paramStr=\([^"]*\).*/\1/p')")
-            redirect_url=$(curl -Ls -w "%{url_effective}" -o /dev/null -F "paramStr=$paramstr" -F "UserName=$username" -F "PassWord=$password" "$auth_url/authServlet")
+            redirect_url=$(curl -Ls -w "%{url_effective}" -o /dev/null -X POST "$auth_url/authServlet" --data-urlencode "paramStr=$paramstr" --data-urlencode "UserName=$username" --data-urlencode "PassWord=$password")
             if [[ "$redirect_url" == *"logon.jsp"* ]];then
                 log "Auth Pass" "paramstr=$paramstr"  
                 return 0
